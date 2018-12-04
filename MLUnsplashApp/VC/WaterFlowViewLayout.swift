438120d6cfa69b2526d9cf6a16ad97fe4b969ff7
mileyliu325 <smileyliu325@gmail.com>
2018-12-04 11:38:23 +1100

    init freamwork

---
+++ MLUnsplashApp/VC/WaterFlowViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  WaterFlowViewLayout.swift
+//  MLUnsplashApp
+//
+//  Created by Simeng Liu on 4/12/18.
+//  Copyright © 2018 Simeng Liu. All rights reserved.
+//
+
+import UIKit
+
+class WaterFlowViewLayout: UICollectionViewLayout {
+
+}
